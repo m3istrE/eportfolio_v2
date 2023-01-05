d3d1e9822957517348cc98cf3aa4c612a3a5421e
m3istrE <v-2krisg@outlook.com>
2023-01-04 22:25:26 -0500

    commit overlay js

--- index.js
+++ index.js
@@ -3,9 +3,18 @@
 // UserID: jVsHuaxsPFTEcB-o_
 
 function contact(event) {
-    event.preventDefault();
-    emailjs
-      .sendForm('service_h3lv0ku', 'template_tmumtro',event.target,'jVsHuaxsPFTEcB-o_').then(() => {
-        console.log('SUCCESS!');
-      })
+    // event.preventDefault();
+    // emailjs
+    //   .sendForm('service_h3lv0ku', 'template_tmumtro',event.target,'jVsHuaxsPFTEcB-o_').then(() => {
+    //     console.log('SUCCESS!');
+    //   })
+
+    const loading = document.querySelector('.modal__overlay--loading');
+    const success = document.querySelector('.modal__overlay--success');
+    loading.classList += " modal__overlay--visible";
+    setTimeout(() => {
+      loading.classList.remove("modal__overlay--visible");
+      success.classList += " modal__overlay--visible";
+      console.log('SUCCESS!');
+    }, 1000);
 }
