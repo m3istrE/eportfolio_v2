c40813b5f3179404fe77bbb2cdb84c65a497ad5e
m3istrE <v-2krisg@outlook.com>
2023-01-08 00:35:12 -0500

    fixed refresh issue when submitting email

--- index.js
+++ index.js
@@ -24,22 +24,30 @@
 //   })
 
 function contact(event) {
+  event.preventDefault();
   const loading = document.querySelector('.modal__overlay--loading');
   const success = document.querySelector('.modal__overlay--success');
 
   loading.classList.toggle("modal__overlay--visible");
 
+  emailjs
+  .sendForm('service_h3lv0ku', 'template_tmumtro',event.target,'jVsHuaxsPFTEcB-o_').then(() => {
+    loading.classList.toggle("modal__overlay--visible");
+    success.classList.toggle("modal__overlay--visible");
+  })
+
   // submit the form and handle the response
   // submitForm().then(() => {
   //   loading.classList.toggle("modal__overlay--visible");
+  //   console.log('SUCCESS!');
   //   success.classList.toggle("modal__overlay--visible");
   // }).catch(() => {
   //   loading.classList.toggle("modal__overlay--visible");
   //   // show an error message instead of the success message
   // });
-  setTimeout(() => {
-    loading.classList.toggle("modal__overlay--visible");
-    success.classList.toggle("modal__overlay--visible");
-    console.log('SUCCESS!');
-  }, 1000);
+  // setTimeout(() => {
+  //   loading.classList.toggle("modal__overlay--visible");
+  //   success.classList.toggle("modal__overlay--visible");
+  //   console.log('SUCCESS!');
+  // }, 1000);
 }
