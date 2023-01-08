5eff6ca4048dd3efefd81093f6e44ec7433917f1
m3istrE <v-2krisg@outlook.com>
2023-01-08 00:16:33 -0500

    commit send toggle

--- index.js
+++ index.js
@@ -2,19 +2,44 @@
 // ServiceID: service_h3lv0ku
 // UserID: jVsHuaxsPFTEcB-o_
 
-function contact(event) {
-    // event.preventDefault();
-    // emailjs
-    //   .sendForm('service_h3lv0ku', 'template_tmumtro',event.target,'jVsHuaxsPFTEcB-o_').then(() => {
-    //     console.log('SUCCESS!');
-    //   })
+// function contact(event) {
 
-    const loading = document.querySelector('.modal__overlay--loading');
-    const success = document.querySelector('.modal__overlay--success');
-    loading.classList += " modal__overlay--visible";
-    setTimeout(() => {
-      loading.classList.remove("modal__overlay--visible");
-      success.classList += " modal__overlay--visible";
-      console.log('SUCCESS!');
-    }, 1000);
+//     const loading = document.querySelector('.modal__overlay--loading');
+//     console.log('loading')
+//     const success = document.querySelector('.modal__overlay--success');
+//     console.log('success')
+//     loading.classList += " modal__overlay--visible";
+//     console.log('loading');
+//     setTimeout(() => {
+//       loading.classList.remove(" modal__overlay--visible");
+//       success.classList += " modal__overlay--visible";
+//       console.log('SUCCESS!');
+//     }, 1000);
+// }
+
+// event.preventDefault();
+// emailjs
+//   .sendForm('service_h3lv0ku', 'template_tmumtro',event.target,'jVsHuaxsPFTEcB-o_').then(() => {
+//     console.log('SUCCESS!');
+//   })
+
+function contact(event) {
+  const loading = document.querySelector('.modal__overlay--loading');
+  const success = document.querySelector('.modal__overlay--success');
+
+  loading.classList.toggle("modal__overlay--visible");
+
+  // submit the form and handle the response
+  // submitForm().then(() => {
+  //   loading.classList.toggle("modal__overlay--visible");
+  //   success.classList.toggle("modal__overlay--visible");
+  // }).catch(() => {
+  //   loading.classList.toggle("modal__overlay--visible");
+  //   // show an error message instead of the success message
+  // });
+  setTimeout(() => {
+    loading.classList.toggle("modal__overlay--visible");
+    success.classList.toggle("modal__overlay--visible");
+    console.log('SUCCESS!');
+  }, 1000);
 }
