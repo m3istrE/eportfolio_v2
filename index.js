1e1eaecbf38c83ae6e1545a4d290024f0f8b54f4
m3istrE <v-2krisg@outlook.com>
2023-01-29 23:43:16 -0500

    commit dark-theme

--- index.js
+++ index.js
@@ -2,23 +2,40 @@
 // ServiceID: service_h3lv0ku
 // UserID: jVsHuaxsPFTEcB-o_
 
-function contact(event) {
+// function contact(event) {
+//   event.preventDefault();
+//   const loading = document.querySelector('.modal__overlay--loading');
+//   const success = document.querySelector('.modal__overlay--success');
+
+//   loading.classList.toggle("modal__overlay--visible");
+
+//   emailjs
+//   .sendForm('service_h3lv0ku', 'template_tmumtro',event.target,'jVsHuaxsPFTEcB-o_').then(() => {
+//     loading.classList.toggle("modal__overlay--visible");
+//     success.classList.toggle("modal__overlay--visible");
+//   }).catch(() => {
+//     loading.classList.toggle("modal__overlay--visible");
+//     alert(
+//       "The email service is temporarily unavailable. Contact me directly on v-2krisg@outlook.com"
+//     )
+//   })
+// }
+
+async function contact(event) {
   event.preventDefault();
   const loading = document.querySelector('.modal__overlay--loading');
   const success = document.querySelector('.modal__overlay--success');
 
   loading.classList.toggle("modal__overlay--visible");
 
-  emailjs
-  .sendForm('service_h3lv0ku', 'template_tmumtro',event.target,'jVsHuaxsPFTEcB-o_').then(() => {
+  try {
+    await emailjs.sendForm('service_h3lv0ku', 'template_tmumtro', event.target, 'jVsHuaxsPFTEcB-o_');
     loading.classList.toggle("modal__overlay--visible");
     success.classList.toggle("modal__overlay--visible");
-  }).catch(() => {
+  } catch (error) {
     loading.classList.toggle("modal__overlay--visible");
-    alert(
-      "The email service is temporarily unavailable. Contact me directly on v-2krisg@outlook.com"
-    )
-  })
+    alert("The email service is temporarily unavailable. Contact me directly on v-2krisg@outlook.com");
+  }
 }
 
 
@@ -26,6 +43,8 @@ function toggleModal(event) {
   document.body.classList.toggle("modal--open");
 }
 
-function toggleContrast(event){
+function toggleContrast(event) {
   document.body.classList.toggle("dark-theme");
 }
+
+//how to use async instead of then for sending an email?
